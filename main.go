ce0720b056a148ea6643592b7364bd7059d27f39
Daniel Palma <danpalma94@gmail.com>
2024-10-25 09:52:44 -0600

    added json storage

--- main.go
+++ main.go
@@ -6,9 +6,9 @@ func main() {
 	fmt.Println("Inside main in todo app")
 
 	todos := Todos{}
-	todos.add("Buy milk")
-	todos.add("Buy bread")
-	todos.toggle(0)
+	storage := NewStorage[Todos]("todos.json")
+	storage.Load(&todos)
 	todos.print()
+	storage.Save(todos)
 
 }
